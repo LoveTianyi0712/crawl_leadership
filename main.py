6df02734759ee137a685d8e099c08de13df9cc3a
LoveTianyi <vocaloidluotianyi@qq.com>
2024-08-26 10:45:02 +0800

    Add database connections

--- main.py
+++ main.py
@@ -1,7 +1,48 @@
 from crawl_website import crawl_website
+import pymysql
 
 if __name__ == '__main__':
     period = 7
     leadership_list = crawl_website(period)
+
+    # SQL Connection
+    conn = pymysql.connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        password='root',
+        database='test',
+        charset='utf8mb4',
+        connect_timeout=10
+    )
+
+    cur = conn.cursor()
+
+    # Create table
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Leadership (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255),
+            gender VARCHAR(10),
+            race VARCHAR(50),
+            birthdate VARCHAR(20),
+            political_status VARCHAR(50),
+            education VARCHAR(50),
+            current_pos TEXT,
+            prepare_pos TEXT
+        )
+    ''')
+
+    # Insert data into database
     for leadership in leadership_list:
-        print(leadership.to_dict())
+        cur.execute('''
+        INSERT INTO Leadership (name, gender, race, birthdate, political_status, education, current_pos, prepare_pos)
+        VALUES (%(name)s, %(gender)s, %(race)s, %(birthdate)s, %(political_status)s, %(education)s, %(current_pos)s, %(prepare_pos)s)
+        ''', leadership.to_dict())
+
+    # Commit the query
+    conn.commit()
+
+    # Close the database connections
+    cur.close()
+    conn.close()
